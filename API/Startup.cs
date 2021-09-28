fa26e84ca80448a99112b7a2ebac3d418c8d2128
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-09-28 19:38:19 -0300

    Adiciona Cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(
+                options =>
+                {
+                    options.AddPolicy("CorsPolicy", builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                }
+            );            
+
             services.AddDbContext<DataContext>(
                 options => options.UseInMemoryDatabase("database")
             );
@@ -49,6 +59,8 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
